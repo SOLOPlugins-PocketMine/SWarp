f64c34c3b7c5ba334e408e206b294c6fa7905b43
solo5star <solo_5@naver.com>
2018-01-01 17:40:36 +0900

    WarpRemoveCommand: Use try-catch

--- src/solo/swarp/command/WarpRemoveCommand.php
+++ src/solo/swarp/command/WarpRemoveCommand.php
@@ -25,30 +25,17 @@ class WarpRemoveCommand extends Command{
       $sender->sendMessage(SWarp::$prefix . "이 명령을 실행할 권한이 없습니다.");
       return true;
     }
-
     if(empty($args)){
       $sender->sendMessage(SWarp::$prefix . "사용법 : " . $this->getUsage() . " - " . $this->getDescription());
       return true;
     }
-    $warpName = $args[0];
 
-    $warp = $this->owner->getWarp($warpName);
-
-    if(!$warp instanceof Warp){
-      $sender->sendMessage(SWarp::$prefix . "\"" . $warpName . "\" 은(는) 존재하지 않는 워프입니다.");
-      return true;
+    try{
+      $this->owner->removeWarp(array_shift($args));
+    }catch(WarpException $e){
+      $sender->sendMessage(SWarp::$prefix . $e->getMessage());
     }
-    $ev = new WarpRemoveEvent($warp);
-    $this->owner->getServer()->getPluginManager()->callEvent($ev);
-    if($ev->isCancelled()){
-      $sender->sendMessage(SWarp::$prefix . "워프 제거에 실패하였습니다.");
-      return true;
-    }
-    $this->owner->removeWarp($warp->getName());
-
     $sender->sendMessage(SWarp::$prefix . "\"" . $warp->getName() . "\" 워프를 제거하였습니다.");
-
-    $this->owner->save(); //save data
     return true;
   }
 }
