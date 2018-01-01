b23c7d1d8656704f3fc9a35277ff4c39a4b649f1
solo5star <solo_5@naver.com>
2018-01-01 19:54:56 +0900

    WarpOption: serialize properties

--- src/solo/swarp/WarpOption.php
+++ src/solo/swarp/WarpOption.php
@@ -17,12 +17,15 @@ abstract class WarpOption{
   }
 
   public function jsonSerialize() : array{
-    return [];
+    return get_object_vars($this);
   }
 
   public static function jsonDeserialize(array $data) : WarpOption{
     $ref = new \ReflectionClass(static::class);
     $option = $ref->newInstanceWithoutConstructor();
+    foreach($data as $key => $value){
+      $option->{$key} = $value;
+    }
     return $option;
   }
 }
